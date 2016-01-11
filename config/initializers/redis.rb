f578844ad124fd795de0e2d76ea384bcf13add03
Jamie Barton <jamie@codethisway.com>
2016-01-11 16:14:09 +0000

    Fixed sidekiq concurrency issues with redis

--- config/initializers/redis.rb
+++
@@ -1,8 +0,0 @@
-if ENV["REDIS_TO_GO"]
-  uri = URI.parse(Rails.application.secrets.redis_url)
-  REDIS = Redis.new(
-    host: uri.host,
-    port: uri.port,
-    password: uri.password
-  )
-end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 require 'sidekiq'
 
-Sidekiq.configure_server do |config|
-  config.redis = { url: "#{Rails.application.secrets.redis_url}"}
+Sidekiq.configure_client do |config|
+  config.redis = { url: "#{Rails.application.secrets.redis_url}", size: 7 }
 end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: "#{Rails.application.secrets.redis_url}"}
+Sidekiq.configure_server do |config|
+  config.redis = { url: "#{Rails.application.secrets.redis_url}", size: 3 }
 end
 
 module Sidekiq
