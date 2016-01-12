1751a7a191a2e134c1c35b597bdea8cb4f05a323
Jamie Barton <jamie@codethisway.com>
2016-01-12 13:30:46 +0000

    New form form almost complete

--- app/assets/javascripts/app/components/form/new/form.controller.js
+++ app/assets/javascripts/app/components/form/new/form.controller.js
@@ -8,6 +8,21 @@
 
   function NewFormController () {
     var vm = this;
-    vm.message = 'Hello Jamie';
+    vm.formData = {};
+
+    vm.optionalEmails = [];
+    vm.newOptionalEmail = null;
+    vm.addOptionalEmail = function (isValid) {
+      if (isValid) {
+        vm.optionalEmails.push(vm.newOptionalEmail);
+        vm.newOptionalEmail = null;
+      }
+    }
+
+    vm.createForm = function (isValid) {
+      if (isValid) {
+        console.log(vm.formData);
+      }
+    }
   }
 })();
