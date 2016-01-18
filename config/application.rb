a8ba0d030b6a7c0af586aa9ccd788762b1fe8912
Jamie Barton <jamie@codethisway.com>
2016-01-18 16:31:10 +0000

    Updated CORS domains

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module Walter
 
     config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
       allow do
-        origins 'http://localhost:3003'
+        origins 'http://localhost:3003', 'http://getwalter.io'
         resource '*', headers: :any, methods: [:get, :post, :options]
       end
     end
