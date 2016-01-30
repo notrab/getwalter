54973d18108e51d2d09991b39f2ab843713b975c
Jamie Barton <jamie@codethisway.com>
2016-01-30 23:40:06 +0000

    Improved ApplicationController Authorization methods
    It was pretty hard to maintain previously. I've split the methods out to
    make things a little clearer for maintenance.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   require 'auth_token'
 
   protect_from_forgery with: :null_session
-  before_action :authenticate, except: [:welcome]
+  before_action :authenticate!, except: [:welcome]
 
   def welcome
     render json: {
@@ -18,14 +18,24 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def authenticate
+  def authenticate!
     begin
-      token = request.headers['Authorization'].split(' ').last
-      payload, header = AuthToken.valid?(token)
-      @current_user = User.find_by(id: payload['user_id'])
+      @current_user = User.find_by(id: payload_user_id)
+    rescue ActiveRecord::RecordNotFound
+      render json: {error: 'User not found.'},
+        status: :unauthorized
     rescue
       render json: {error: 'Authorization header not valid'},
         status: :unauthorized
     end
+  end
+
+  def payload_user_id
+    AuthToken.valid?(bearer_token)['user_id']
+  end
+
+  def bearer_token
+    header = request.headers['Authorization']
+    header ? auth_header.split(' ').last : nil
   end
 end
